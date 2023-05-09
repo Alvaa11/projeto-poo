2531ec977af84ffb54a10febf7c6f5d49f9a07d3
alvaa11.ofc <alvaro02.rodrigues@gmail.com>
2023-05-09 10:48:54 -0300

    corrige o erro do Alterar

--- factory/excel_factory.py
+++ factory/excel_factory.py
@@ -27,15 +27,21 @@ class AlterarFactory:
 
     @staticmethod
     def Alterar() -> Funcionario:
-        planilha = input('Qual planilha? ')
-        planilha = op.load_workbook(f'{planilha}.xlsx')
-        pagina = input('Qual pagina está o que deve ser alterado? ')
-        pagina = planilha[f'{pagina}']
-        alterar = input('O que devemos alterar nessa pagina? ')
-        alteracao = input('Pelo que quer alterar? ')
-        for rows in pagina.iter_rows(min_row=1, max_row=1000):
-            for cell in rows:
-                if cell.value == alterar:
-                    cell.value = alteracao
-        planilha.save(f'{planilha}.xlsx')
-        return Funcionario(IAlterarExcelRepository.Alter_sheet(planilha=planilha, pagina=pagina, substituicao=alterar, substituto=alteracao))
+        try:    
+            planilha = input('Qual planilha? ')
+            planilha = op.load_workbook(f'{planilha}.xlsx')
+            pagina = input('Qual pagina está o que deve ser alterado? ')
+            pagina = planilha[f'{pagina}']
+            alterar = input('O que devemos alterar nessa pagina? ')
+            alteracao = input('Pelo que quer alterar? ')
+            for rows in pagina.iter_rows(min_row=1, max_row=1000):
+                for cell in rows:
+                    if cell.value == alterar:
+                        cell.value = alteracao
+            save = input('Como deseja salvar? ')
+            planilha.save(f'{save}.xlsx')
+            return Funcionario(AlterarExcelRepository.Alter_sheet(planilha=planilha, pagina=pagina, substituicao=alterar, substituto=alteracao))
+        except FileNotFoundError as error:
+            print(f'{error}, Arquivo não encontrado')
+        except KeyError:
+            print('Por favor, verifique se os dados estão certos.')            
